0337e669f9bc5b1da6aa22445e354847932124d6
junlee91 <jlee757@uic.edu>
2018-08-29 21:13:42 -0500

    Fairs component base

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Router, Scene, Stack } from 'react-native-router-flux';
 
+import Fairs from './components/Fairs';
 import CompanyList from './components/CompanyList';
 import CompanyDetail from './components/CompanyDetail';
 
 const RouterComponent = () => (
   <Router>
     <Stack key="root">
-      <Scene key="companyList" initial component={CompanyList} title="Employer List" />
+      <Scene key="fairs" initial component={Fairs} title="Career Fairs" />
+      <Scene key="companyList" component={CompanyList} title="Employer List" />
       <Scene key="companyDetail" component={CompanyDetail} title="Employer Detail" />
     </Stack>
   </Router>
