e8c731eb8fa49971c641f1d5246d34322514fbd3
junlee91 <jlee757@uic.edu>
2018-08-31 21:23:00 -0500

    company logo sizing fixed

--- src/components/CompanyDetail/presenter.js
+++ src/components/CompanyDetail/presenter.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Divider } from 'react-native-elements';
+import styled from 'styled-components';
+
+import { LogoImage } from '../commons';
+
 
 const CompanyDetail = (props) => {
   const { companyInfo } = props;
 
   return (
-    <View>
-      <Text>{companyInfo.name}</Text>
-    </View>
+    <Container>
+      <InfoBox>
+        <View style={styles.titleContent}>
+          <LogoImage {...companyInfo} size="medium" />
+        </View>
+      </InfoBox>
+      <InfoBox>
+        <Text>Event Info.</Text>
+      </InfoBox>
+      <InfoBox>
+        <Text>Detail Info.</Text>
+      </InfoBox>
+      <InfoBox>
+        <Text>Actions</Text>
+      </InfoBox>
+    </Container>
   );
 };
+
+const Container = styled.View`
+  display: flex;
+  flex-direction: column;
+`;
+
+const InfoBox = styled.View`
+  border: 1px solid #e6e6e6;
+  background-color: white;
+  box-shadow: 0 8px 38px rgba(209, 209, 209, 0.445);
+  border-radius: 5px;
+  padding: 15px;
+  display: flex;
+`;
+
+const styles = StyleSheet.create({
+  titleContent: {
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    alignItems: 'center',
+  },
+});
 
 export default CompanyDetail;
--- src/components/commons/LogoImage.js
+++ src/components/commons/LogoImage.js
@@ -2,20 +2,47 @@ import React from 'react';
 import FadeIn from 'react-native-fade-in-image';
 import { Image } from 'react-native';
 
-const LogoImage = props => (
-  <FadeIn>
-    <Image
-      source={{ uri: `https://logo.clearbit.com/${props.company_url}/` }}
-      style={styles.imgStyle}
-      defaultSource={require('../../img/no_img.png')}
-    />
-  </FadeIn>
-);
+function getSize(size) {
+  switch (size) {
+    case 'small':
+      return 64;
+    case 'medium':
+      return 128;
+    case 'large':
+      return 256;
+    default:
+      return 128;
+  }
+}
+
+const LogoImage = (props) => {
+  const size = getSize(props.size);
+  const imgStyle = size === 64 ? styles.imgStyleSmall : styles.imgStyleMedium;
+  const uri = `https://logo.clearbit.com/${props.company_url}?size=${size}`;
+
+  return (
+    <FadeIn>
+      <Image
+        source={{ uri }}
+        style={imgStyle}
+        defaultSource={require('../../img/no_img.png')}
+      />
+    </FadeIn>
+  );
+};
 
 const styles = {
-  imgStyle: {
-    height: 50,
-    width: 50
+  imgStyleSmall: {
+    height: 64,
+    width: 64,
+    alignSelf: 'center',
+    resizeMode: 'stretch',
+  },
+  imgStyleMedium: {
+    height: 128,
+    width: 128,
+    alignSelf: 'center',
+    resizeMode: 'stretch',
   }
 };
 
