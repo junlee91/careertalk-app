34dda0e936d41e5bd7b95b1218d499ed62234a17
junlee91 <jlee757@uic.edu>
2018-09-02 15:55:33 -0500

    fav list redux

--- src/components/CompanyDetail/container.js
+++ src/components/CompanyDetail/container.js
@@ -3,28 +3,28 @@ import React, { Component } from 'react';
 import CompanyDetail from './presenter';
 
 class Container extends Component {
-  state = {
-    isLiked: false
-  };
-
   componentWillMount() {
-    const {
-      fairs: { Careerfair },
-      companyInfo
-    } = this.props;
+    const { fairs: { Careerfair }, companyInfo, favorites } = this.props;
     const fair = Careerfair.filter(fair => fair.id === companyInfo.fair_id);
     const { start_date_min } = fair[0];
+    const isLiked = favorites.includes(companyInfo.id);
 
     this.setState({
       date: start_date_min,
       companyInfo,
+      isLiked,
     });
   }
 
   _handleLike = () => {
     const { isLiked, companyInfo } = this.state;
+    const { likeCompany, unlikeCompany } = this.props;
 
-    console.log(`Just liked ${companyInfo.id}!!`);
+    if (!isLiked) {
+      likeCompany(companyInfo.id);
+    } else {
+      unlikeCompany(companyInfo.id);
+    }
 
     this.setState({
       isLiked: !isLiked
--- src/components/CompanyDetail/index.js
+++ src/components/CompanyDetail/index.js
@@ -1,11 +1,23 @@
 import { connect } from 'react-redux';
 import Container from './container';
+import { actionCreators as userActions } from '../../redux/modules/user';
+
 
 const mapStateToProps = (state) => {
-  const { user: { fairs } } = state;
+  const { user: { fairs, favorites } } = state;
   return {
-    fairs
+    fairs,
+    favorites
   };
 };
 
-export default connect(mapStateToProps)(Container);
+const mapDispatchToProps = dispatch => ({
+  likeCompany: (cmpId) => {
+    dispatch(userActions.likeCompany(cmpId));
+  },
+  unlikeCompany: (cmpId) => {
+    dispatch(userActions.unlikeCompany(cmpId));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Container);
