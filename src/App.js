d0a153b21ad563a8f1a64871e1a8b50bf11e01e4
junlee91 <jlee757@uic.edu>
2018-09-05 21:24:00 -0500

    xcode settings & login page commented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 
 import Router from './Router';
 import configureStore from './redux/configureStore';
@@ -8,6 +9,10 @@ import configureStore from './redux/configureStore';
 const { persistor, store } = configureStore();
 
 class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <Provider store={store}>
