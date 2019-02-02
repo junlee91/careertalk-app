0bed328c226fe0a243abb2840fb9a6dc6ffed626
junlee91 <jlee757@uic.edu>
2019-02-02 14:29:42 -0600

    Alert modal for update

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { Alert, Linking, Platform } from 'react-native';
 
 import { PublicRouter, PrivateRouter } from './Router';
+import { Spinner } from './components/commons';
+import config from '../config.json';
 
 const mapStateToProps = (state) => {
   const { auth: { isLoggedIn } } = state;
@@ -12,9 +15,38 @@ const mapStateToProps = (state) => {
 };
 
 class App extends Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    Alert.alert(
+      'New version is available!',
+      'Follow the link to update the app.',
+      [
+        {
+          text: 'Update',
+          onPress: () => {
+            Linking.openURL(Platform.OS === 'ios' ? config.app_store : config.google_play);
+
+            // TODO: move this method to componentWillMount
+            this.setState({ loading: false });
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+
+    console.log(`Current Version is ${config.version}`);
+  }
+
   render() {
-    return this.props.isLoggedIn ? <PrivateRouter /> : <PublicRouter />;
+    const { loading } = this.state;
+
+    return <Fragment>{loading ? <Spinner size="large" /> : <Router {...this.props} />}</Fragment>;
   }
 }
+
+const Router = props => (props.isLoggedIn ? <PrivateRouter /> : <PublicRouter />);
 
 export default connect(mapStateToProps)(App);
