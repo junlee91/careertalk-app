bb89971fb89ab0cf98b35249c06be32486c753e2
junlee91 <jlee757@uic.edu>
2019-08-02 23:12:00 -0500

    Apollo Client set

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Text, View, AsyncStorage } from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import SplashScreen from 'react-native-splash-screen';
 
+import { ApolloProvider } from 'react-apollo-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import ApolloClient from 'apollo-boost';
+
 import CareerTalk from './src/CareerTalk';
+import apolloOptions from './Apollo';
 
 const theme = {
   ...DefaultTheme,
@@ -15,13 +22,47 @@ const theme = {
 };
 
 export default () => {
-  useEffect(() => {
-    SplashScreen.hide();
-  });
+  const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const preLoad = async () => {
+    try {
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage // TODO: this is deprecated
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloOptions
+      });
+      setLoaded(true);
+      setClient(client);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  return (
-    <PaperProvider theme={theme}>
-      <CareerTalk />
-    </PaperProvider>
+  useEffect(() => {
+    preLoad();
+    SplashScreen.hide();
+  }, []);
+
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <PaperProvider theme={theme}>
+        <CareerTalk />
+      </PaperProvider>
+    </ApolloProvider>
+  ) : (
+    <View
+      style={{
+        minHeight: 500,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}
+    >
+      <Text>Loading...</Text>
+    </View>
   );
 };
