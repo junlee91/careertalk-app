36fa161fbaa5de7abed635b92c88cdb9a615eba7
junlee91 <jlee757@uic.edu>
2019-09-22 15:20:56 -0500

    Final Release

--- src/CareerTalk.js
+++ src/CareerTalk.js
@@ -1,63 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Alert, Linking, Platform } from 'react-native';
+import React, { useState } from 'react';
 
 import { PublicRouter, PrivateRouter } from './Router';
-import { Spinner } from './components/commons';
-import config from '../config.json';
-import pjson from '../package.json';
 
 /** CareerTalk using React Hooks and Graphql */
 const CareerTalk = ({ isLoggedIn }) => {
-  const [loading, setLoading] = useState(true);
   const [isLoggedInState, setIsLoggedInState] = useState(isLoggedIn);
 
-  const checkVersion = () => {
-    fetch(`${config.API_URL}/careertalk/version`)
-      .then(resp => resp.json())
-      .then(json => {
-        if (json.version === pjson.version) {
-          setLoading(false);
-        } else {
-          Alert.alert(
-            'New version is available!',
-            'Follow the link to update the app.',
-            [
-              {
-                text: 'Update',
-                onPress: () => {
-                  Linking.openURL(Platform.OS === 'ios' ? config.app_store : config.google_play);
-                }
-              }
-            ],
-            { cancelable: false }
-          );
-        }
-      });
-  };
-
-  useEffect(() => {
-    checkVersion();
-  });
-
-  return (
-    <>
-      {loading ? (
-        <View
-          style={{
-            minHeight: 500,
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column'
-          }}
-        >
-          <Spinner size="large" />
-          <Text>Version checking...</Text>
-        </View>
-      ) : (
-        <Router isLoggedInState={isLoggedInState} setIsLoggedInState={setIsLoggedInState} />
-      )}
-    </>
-  );
+  return <Router isLoggedInState={isLoggedInState} setIsLoggedInState={setIsLoggedInState} />;
 };
 
 const Router = ({ isLoggedInState, setIsLoggedInState }) => (isLoggedInState ? (
